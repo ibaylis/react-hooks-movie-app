356eef7bcee8c02a27ff231d99d657c656c10cd4
Ian Baylis <ibaylis@USMDWK315.local>
2019-03-08 18:51:45 -0500

    Movie App with React Hooks and Styled Components

---
+++ src/components/styles/StyledPlaylist.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+const StyledPlaylist = styled.div`
+  flex: 1 1 450px;
+  overflow: hidden;
+  color: white;
+
+  @media screen and (max-width: 1400px) {
+    width: 10%;
+    display: block;
+  }
+`;
+
+export default StyledPlaylist;
+
+
+
+
+
+
+
+
+// import styled from "styled-components";
+
+// const StyledPlaylist = styled.div`
+//   -webkit-box-flex: 1;
+//   -ms-flex: 1 1 450px;
+//   flex: 1 1 450px;
+//   overflow: hidden;
+//   color: white;
+//   /* height: 0;
+//   padding-top:50px;
+//   padding-bottom: 36%; */
+
+//   @media screen and (max-width: 1400px) {
+//     width: 100%;
+//     display: block;
+//   }
+// `;
+
+// export default StyledPlaylist;
